ea327f0e8fd3e5f74d11c00e98afa0080ecf0e0b
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-29 18:30:32 +0300

    test: Implement signup functionality in SignupViewModel
    This commit adds the implementation of the `signUpWithEmail` method in the `SignupViewModel` class. The method validates the form inputs and calls the `Signup` use case to create a new user with the provided signup parameters. It uses the `AuthRepository` to handle the signup process, including checking for network connectivity, calling the remote data source, and caching the user data locally. This feature enhances the signup experience for users.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature or enhancement.

--- lib/features/auth/presentation/screens/view model/signup view model/signup_view_model.dart
+++ lib/features/auth/presentation/screens/view model/signup view model/signup_view_model.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:notify/core/network/network_info.dart';
 import 'package:notify/core/routers/app_routers_enum.dart';
+import 'package:notify/core/utils/shared%20preferances/shared_preferences.dart';
 import 'package:notify/core/utils/validators/base_validator.dart';
 import 'package:notify/core/utils/validators/equal_to_validator.dart';
 import 'package:notify/core/utils/validators/longer_than_2_chars.dart';
 import 'package:notify/core/utils/validators/no_space_validator.dart';
 import 'package:notify/core/utils/validators/required_validator.dart';
+import 'package:notify/features/auth/data/data_source/local/local_data_source_impl.dart';
+import 'package:notify/features/auth/data/data_source/local/local_data_sourece.dart';
+import 'package:notify/features/auth/data/data_source/remote_data_source.dart';
+import 'package:notify/features/auth/data/data_source/remote_data_source_impl.dart';
+import 'package:notify/features/auth/data/repository/auth_repo_impl.dart';
+import 'package:notify/features/auth/domin/repository/auth_repository.dart';
+import 'package:notify/features/auth/domin/usecases/signup.dart';
 import 'package:notify/features/auth/presentation/screens/view model/auth_view_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SignupViewModle extends AuthViewModel with Icons, Strings, Sizes, Validators {
+class SignupViewModle extends AuthViewModel
+    with Icons, Strings, Sizes, Validators {
   @override
   String get stringGoTo => "Login";
 
@@ -30,7 +41,7 @@ class SignupViewModle extends AuthViewModel with Icons, Strings, Sizes, Validato
   late TextEditingController emailController;
   late TextEditingController passwordController;
   late TextEditingController confirmPasswordController;
-  void signUpWithEmail() {
+  Future<void> signUpWithEmail() async {
     if (formKey.currentState!.validate()) {
       print("email : ${emailController.text.toLowerCase().trim()}");
       print("password : ${passwordController.text}");
@@ -38,8 +49,35 @@ class SignupViewModle extends AuthViewModel with Icons, Strings, Sizes, Validato
       print("last name : ${lastNameController.text}");
       print("username : ${usernameController.text}");
       print("confirm password : ${confirmPasswordController.text}");
-      
+      SharedPreferences sharedPreferncesServices =
+          await SharedPreferences.getInstance();
 
+      SharedPreferencesServices sharedPreferencesServices =
+          SharedPreferencesServices(
+              sharedPreferences: sharedPreferncesServices);
+      AuthLocalDataSource authLocalDataSourceImpl = AuthLocalDataSourceImp(
+          saveDataLocal: sharedPreferencesServices);
+      AuthRemoteDataSource authRemoteDataSourceImpl =
+          AuthRemoteDataSourceImpl();
+      NetworkInfo networkInfo = NetworkInfoImpl();
+      AuthRepository authRepositoryImpl = AuthRepositoryImpl(
+          localDataSource: authLocalDataSourceImpl,
+          remoteDataSource: authRemoteDataSourceImpl,
+          networkInfo: networkInfo);
+
+      final params = SignUpParams(
+        firstName: firstNameController.text,
+        lastName: lastNameController.text,
+        userName: usernameController.text,
+        email: emailController.text.toLowerCase().trim(),
+        password: passwordController.text,
+      );
+      final result = await Signup(authRepositoryImpl).call(params);
+      result.fold((l) {
+        print(l);
+      }, (r) {
+        print(r);
+      });
     }
   }
 }
@@ -72,16 +110,15 @@ mixin Validators {
       BaseValidator.validateValue(
         context,
         value ?? "",
-        [RequiredValidator(), LognerThan2Chars(),NoSpaceValidator()],
+        [RequiredValidator(), LognerThan2Chars(), NoSpaceValidator()],
         true,
       );
-  String? confirmPasswordValidator(String? value, BuildContext context,String password) =>
+  String? confirmPasswordValidator(
+          String? value, BuildContext context, String password) =>
       BaseValidator.validateValue(
         context,
         value ?? "",
         [EqualToPasswordValidator(password)],
         true,
       );
-
-
 }
