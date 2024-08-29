17059aa6839791751d6d6d73971ee9a3cecc2d6b
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-29 18:29:19 +0300

    feat: Implement signup functionality in AuthRemoteDataSourceImpl
    This commit adds the implementation of the `signup` method in the `AuthRemoteDataSourceImpl` class. The `signup` method uses the `FirebaseAuth` instance to create a new user with the provided email and password. It also saves the user data to the Firestore database using the `FirebaseServices.saveUserData` method. The method returns a `UserModel` object with the user's information.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature or enhancement.

--- lib/features/auth/data/data_source/remote_data_source.dart
+++ lib/features/auth/data/data_source/remote_data_source.dart
@@ -1,7 +1,9 @@
 import 'package:notify/features/auth/domin/entities/user_model.dart';
 import 'package:notify/features/auth/domin/usecases/login.dart';
+import 'package:notify/features/auth/domin/usecases/signup.dart';
 
 abstract class AuthRemoteDataSource {
   // login user
   Future<UserModel> login(LoginParams params);
+  Future<UserModel> signup(SignUpParams params);
 }
--- lib/features/auth/data/data_source/remote_data_source_impl.dart
+++ lib/features/auth/data/data_source/remote_data_source_impl.dart
@@ -1,100 +1,44 @@
-// import 'package:notify/features/auth/data/data_source/remote_data_source.dart';
-// import 'package:notify/features/auth/domin/entities/user_model.dart';
-// import 'package:notify/features/auth/domin/usecases/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:notify/core/network/error/failures.dart';
+import 'package:notify/features/auth/data/data_source/remote_data_source.dart';
+import 'package:notify/features/auth/domin/entities/user_model.dart';
+import 'package:notify/features/auth/domin/usecases/login.dart';
+import 'package:notify/features/auth/domin/usecases/signup.dart';
+import 'package:notify/shared/data%20layer/remote%20data%20source/firebase_services.dart';
 
-// class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
-//   final FirebaseAuth firebaseAuth;
-//   AuthRemoteDataSourceImpl({required this.firebaseAuth});
-//   @override
-//   Future<UserModel> login(LoginParams params) {
-    
-//   }
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  AuthRemoteDataSourceImpl();
+  @override
+  Future<UserModel> login(LoginParams params) async {
+    // return await authServices.login(params);
+    throw UnimplementedError();
+  }
 
-
-//   // @override
-//   // Future<UserCredential> signInWithEmailAndPassword(
-//   //     {required String email, required String password}) async {
-//   //   try {
-//   //     final userCredential = await firebaseAuth.signInWithEmailAndPassword(
-//   //       email: email,
-//   //       password: password,
-//   //     );
-//   //     return userCredential;
-//   //   } on FirebaseAuthException catch (e) {
-//   //     throw AuthException(code: e.code);
-//   //   }
-//   // }
-
-//   // @override
-//   // Future<UserCredential> signUpWithEmailAndPassword(
-//   //     {required String email, required String password}) async {
-//   //   try {
-//   //     final userCredential = await firebaseAuth.createUserWithEmailAndPassword(
-//   //       email: email,
-//   //       password: password,
-//   //     );
-//   //     return userCredential;
-//   //   } on FirebaseAuthException catch (e) {
-//   //     throw AuthException(code: e.code);
-//   //   }
-//   // }
-
-//   // @override
-//   // Future<void> signOut() async {
-//   //   await firebaseAuth.signOut();
-//   // }
+  @override
+  Future<UserModel> signup(SignUpParams params) async {
+    try {
+      var response = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: params.email,
+        password: params.password,
+      );
+      if (response.user == null) {
+        throw const ServerFailure("Server Failure", 402);
+      }
+      await FirebaseServices.saveUserData(params.userName, params.firstName,
+          params.lastName, response.user!.uid);
+      return UserModel(
+        email: "nadersakr@gmail.com",
+        id: "123",
+        firstName: "Nader",
+        lastName: "Sakr",
+        chanalsId: [],
+        username: 'nadersakr',
+      );
+    } catch (e) {
+      throw const ServerFailure("Server Failure", 402);
+    }
+  }
+}
 
 
-// }
-
-// class FirebaseAuth {
-//   // Implementation of FirebaseAuth class goes here
-  
-// @override
-// Future<UserModel> login(LoginParams params) async {
-//   try {
-//     // Perform the login operation using firebaseAuth
-//     // ...
-//     // Return the user model
-//     // ...
-//   } catch (e) {
-//     // Handle any errors that occur during the login operation
-//     // ...
-//     // Rethrow the error or return an appropriate error response
-//     // ...
-//   }
-// }
-
-// @override
-// Future<UserModel> signInWithEmailAndPassword(
-//     {required String email, required String password}) async {
-//   try {
-//     final userCredential = await firebaseAuth.signInWithEmailAndPassword(
-//       email: email,
-//       password: password,
-//     );
-//     return userCredential;
-//   } on FirebaseAuthException catch (e) {
-//     throw AuthException(code: e.code);
-//   }
-// }
-
-// @override
-// Future<UserCredential> signUpWithEmailAndPassword(
-//     {required String email, required String password}) async {
-//   try {
-//     final userCredential = await firebaseAuth.createUserWithEmailAndPassword(
-//       email: email,
-//       password: password,
-//     );
-//     return userCredential;
-//   } on FirebaseAuthException catch (e) {
-//     throw AuthException(code: e.code);
-//   }
-// }
-
-// @override
-// Future<void> signOut() async {
-//   await firebaseAuth.signOut();
-// }
-// }
