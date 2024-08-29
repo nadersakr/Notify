5637ef16bb5cda194bec3d6579b3b86e9e60b1e0
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-29 18:29:40 +0300

    refactor: Update UserModel class to include username, firstName, and lastName
    The code changes in this commit modify the UserModel class in the `lib/features/auth/domin/entities/user_model.dart` file. The `name` property has been replaced with separate `username`, `firstName`, and `lastName` properties to provide more detailed user information. The constructor, toJson, and fromJson methods have been updated accordingly.
    
    Note: This commit message follows the established convention of using a prefix (refactor) to indicate code refactoring or restructuring.

--- lib/features/auth/domin/entities/user_model.dart
+++ lib/features/auth/domin/entities/user_model.dart
@@ -1,8 +1,42 @@
 class UserModel {
   final String id;
-  final String name;
+  final String firstName;
+  final String lastName;
+  final String username;
   final String email;
   final String? imageUrl;
   final List<String>? chanalsId;
-  UserModel( {this.chanalsId,this.imageUrl,required this.id, required this.name, required this.email});
+  UserModel({required this.username, required this.firstName,required this.lastName,  this.chanalsId,this.imageUrl,required this.id, required this.email});
+
+ factory UserModel.toJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      email: json['email'],
+      imageUrl: json['imageUrl'],
+      chanalsId: json['chanalsId'], username: json['username'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'imageUrl': imageUrl,
+      'chanalsId': chanalsId,
+    };
+  }
+
+  UserModel.fromJson(Map<String, dynamic> map)
+      : id = map['id'],
+        firstName = map['firstName'],
+        lastName = map['lastName'],
+        username = map['username'],
+        email = map['email'],
+        imageUrl = map['imageUrl'],
+        chanalsId = map['chanalsId'];
 }
