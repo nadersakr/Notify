96aff3a355834e91cb870f66ec0c3e0bca93564b
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-29 18:28:29 +0300

    feat: Add custom exceptions for server, cancel token, and cache errors
    Added custom exception classes `ServerException`, `CancelTokenException`, and `CacheException` to handle specific types of errors in the network layer. These exceptions provide more detailed information about the error, such as the error message and status code. This commit adds the initial implementation of these exception classes.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature or enhancement.

---
+++ lib/core/network/error/exceptions.dart
@@ -0,0 +1,37 @@
+/// Exception occur when server failure
+class ServerException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  ServerException(this.message, this.statusCode);
+
+  @override
+  // ignore: hash_and_equals
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    if (other is ServerException) {
+      return other.message == message && other.statusCode == statusCode;
+    }
+
+    return false;
+  }
+}
+
+/// Exception occur when call api over on time
+class CancelTokenException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  CancelTokenException(this.message, this.statusCode);
+}
+
+class CacheException implements Exception {
+  final String message;
+
+  CacheException(this.message);
+}
