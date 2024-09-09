4beb6d0e995ca8f501804f31a42c39e19340bdf0
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-09 17:35:55 +0300

    feat: add signupWIthGoogle method for abstract repo

--- lib/features/auth/domin/repository/auth_repository.dart
+++ lib/features/auth/domin/repository/auth_repository.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:notify/core/network/error/failures.dart';
-import 'package:notify/features/auth/domin/entities/user_model.dart';
+import 'package:notify/shared/domin/entities/user_model.dart';
 import 'package:notify/features/auth/domin/usecases/login.dart';
 import 'package:notify/features/auth/domin/usecases/signup.dart';
 abstract class AuthRepository {
+
   Future<Either<Failure,UserModel>> login(LoginParams params);
   Future<Either<Failure,UserModel>> signup(SignUpParams params);
+  Future<Either<Failure,UserModel>> signinWithGoogle();
 }
