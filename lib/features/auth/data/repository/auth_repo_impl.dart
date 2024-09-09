f3810519b1986492571f67b4e67ee405f7cb783d
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-09 17:35:17 +0300

    error: handling errors for signup method

--- lib/features/auth/data/repository/auth_repo_impl.dart
+++ lib/features/auth/data/repository/auth_repo_impl.dart
@@ -1,11 +1,9 @@
 import 'package:dartz/dartz.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:notify/core/network/error/exceptions.dart';
 import 'package:notify/core/network/error/failures.dart';
 import 'package:notify/core/network/network_info.dart';
 import 'package:notify/features/auth/data/data_source/local/local_data_sourece.dart';
-import 'package:notify/features/auth/data/data_source/remote_data_source.dart';
-import 'package:notify/features/auth/domin/entities/user_model.dart';
+import 'package:notify/features/auth/data/data_source/remote/remote_data_source.dart';
+import 'package:notify/shared/domin/entities/user_model.dart';
 import 'package:notify/features/auth/domin/repository/auth_repository.dart';
 import 'package:notify/features/auth/domin/usecases/login.dart';
 import 'package:notify/features/auth/domin/usecases/signup.dart';
@@ -34,8 +32,27 @@ class AuthRepositoryImpl implements AuthRepository {
         final user = await remoteDataSource.signup(params);
         localDataSource.cacheUser(user);
         return Right(user);
-      } on FirebaseAUthFailure catch (e) {
-        return Left(FirebaseAUthFailure(e.errorMessage));
+      } on FirebaseAuthFailure catch (e) {
+        return Left(FirebaseAuthFailure(e.errorMessage));
+      } on CacheFailure catch (e) {
+        return Left(CacheFailure(e.errorMessage));
+      } catch (e) {
+        return const Left(UnknowFailure("Error Occured"));
+      }
+    } else {
+      return const Left(NetworkFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserModel>> signinWithGoogle() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final user = await remoteDataSource.signinWithGoogle();
+        localDataSource.cacheUser(user);
+        return Right(user);
+      } on FirebaseAuthFailure catch (e) {
+        return Left(FirebaseAuthFailure(e.errorMessage));
       } on CacheFailure catch (e) {
         return Left(CacheFailure(e.errorMessage));
       } catch (e) {
