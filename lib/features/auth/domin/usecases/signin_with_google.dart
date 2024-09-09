ee708ac5e463a55878f314e3e577642a15dbae11
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-09 17:39:57 +0300

    feat: sign in with google usecase

---
+++ lib/features/auth/domin/usecases/signin_with_google.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:notify/core/network/error/failures.dart';
+import 'package:notify/core/utils/usecases/usecase.dart';
+import 'package:notify/shared/domin/entities/user_model.dart';
+import 'package:notify/features/auth/domin/repository/auth_repository.dart';
+
+class SigninWithGoogle extends UseCase<UserModel, NoParams> {
+  final AuthRepository repository;
+
+  SigninWithGoogle(this.repository);
+
+  @override
+  Future<Either<Failure, UserModel>> call(NoParams params) async {
+    final result = await repository.signinWithGoogle();
+    return result.fold((l) {
+      return Left(l);
+    }, (r) async {
+      return Right(r);
+    });
+  }
+}
+
+
