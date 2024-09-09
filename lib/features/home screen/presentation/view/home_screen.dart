b89195f3ec4f6a20d2044b010ec3bf4415d6ce3f
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-09 17:04:36 +0300

    feat: add files for home screen

---
+++ lib/features/home screen/presentation/view/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Home Screen'),
+    );
+  }
+}
