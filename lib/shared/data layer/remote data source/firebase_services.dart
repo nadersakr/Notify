ff9fe7ea057799297403cb4e4a72996caa5ec9cd
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-09 17:44:59 +0300

    feat: initializing firebase , add key file for google authntication

--- lib/shared/data layer/remote data source/firebase_services.dart
+++ lib/shared/data layer/remote data source/firebase_services.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:notify/core/network/error/exceptions.dart';
 import 'package:notify/core/network/error/failures.dart';
-import 'package:notify/features/auth/presentation/screens/view%20model/signup%20view%20model/signup_view_model.dart';
+import 'package:notify/features/auth/presentation/controllers/signup%20view%20model/signup_view_model.dart';
 
 class FirebaseServices {
   static Future<void> saveUserData(
@@ -15,7 +15,7 @@ class FirebaseServices {
     } catch (e) {
       throw CacheException(SignupViewModle.userDatadidnotSaved);
     }
-    
+
     try {
       // Save User data using username
       await FirebaseFirestore.instance
@@ -28,6 +28,7 @@ class FirebaseServices {
       throw CacheException(SignupViewModle.usernameDidnotSaved);
     }
   }
+
   static Future<void> searchUsername(String username) async {
     try {
       final response = await FirebaseFirestore.instance
@@ -35,10 +36,10 @@ class FirebaseServices {
           .doc(username)
           .get();
       if (response.exists) {
-        throw FirebaseAUthFailure(SignupViewModle.usernameAlreadyIn);
+        throw FirebaseAuthFailure(SignupViewModle.usernameAlreadyIn);
       }
     } catch (e) {
-      throw FirebaseAUthFailure(SignupViewModle.usernameAlreadyIn);
+      throw FirebaseAuthFailure(SignupViewModle.usernameAlreadyIn);
     }
   }
 }
