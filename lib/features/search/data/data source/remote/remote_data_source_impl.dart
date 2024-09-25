6ab2aa16470962761579e780f88d01c4b0804237
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-25 05:01:39 +0300

    feat : optimize search funtion

--- lib/features/search/data/data source/remote/remote_data_source_impl.dart
+++ lib/features/search/data/data source/remote/remote_data_source_impl.dart
@@ -5,18 +5,36 @@ import 'package:notify/shared/domin/entities/channel_model.dart';
 
 class SearchRemoteDataSourceImpl implements SearchRemoteDataSource {
   @override
-  Future<List<Channel>> searchForChannel(SearchForChannelParams params) async {
-    final result = await FirebaseFirestore.instance
-        .collection('channels')
-        .where('name', isGreaterThanOrEqualTo: params.query)
-        .where('name',
-            isLessThanOrEqualTo: '${params.query}\uf8ff') // For partial search
-        .get();
-    List<Channel> channels = result.docs
-        .map((e) => Channel.fromFirebase(e))
-        .toList();
+  // Future<List<Channel>> searchForChannel(SearchForChannelParams params) async {
+  //   final result = await FirebaseFirestore.instance
+  //       .collection('channels')
+  //       .where('name', isGreaterThanOrEqualTo: params.query)
+  //       .where('name',
+  //           isLessThanOrEqualTo: '${params.query}\uf8ff') // For partial search
+  //       .get();
+  //   List<Channel> channels = result.docs
+  //       .map((e) => Channel.fromFirebase(e))
+  //       .toList();
 
     
-    return channels;
-  }
+  //   return channels;
+  // }
+
+//  this function can't use for the large data because it fetch all documents
+//  and then filter on client side
+//  so we can use the below function to filter on server side
+//  and then fetch the data
+//  this function is optimizer than the above function, the above function is also correct but is more time consuming
+  Future<List<Channel>> searchForChannel(SearchForChannelParams params) async {
+  final result = await FirebaseFirestore.instance
+      .collection('channels')
+      .get(); // Fetch all documents
+
+  List<Channel> channels = result.docs
+      .map((e) => Channel.fromFirebase(e))
+      .where((channel) => channel.title.toLowerCase().contains(params.query.toLowerCase())) // Filter on client side
+      .toList();
+
+  return channels;
+}
 }
